d7a2131b89022ba85e73b2f59d45ffc71e7b8c91
todd.babbitt <todd.babbitt@outsell.com>
2016-12-09 16:55:02 -0700

    Created additional customer module.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { CustomerComponent } from './customer/customer.component'
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
+  entryComponents: [CustomerComponent]  
 })
 export class AppComponent  
 { 
   name = 'Todd';
   title = 'Angular'; 
   h1Color = 'purple';
-
+  customers = [
+    {id: 1, name: 'ward'},
+    {id: 1, name: 'jim'},
+    {id: 1, name: 'bill'},
+    {id: 1, name: 'suzie'},
+    {id: 1, name: 'jack'}
+  ];
+  
   clickIt()  {
     this.h1Color = this.h1Color === "purple" ? "blue" : "purple";
   }
