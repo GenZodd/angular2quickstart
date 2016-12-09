38b0de34048024b64088f38bca3beb26e8a8f7bd
todd.babbitt <todd.babbitt@outsell.com>
2016-12-09 16:05:13 -0700

    add data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  
+{ 
+  name = 'Todd';
+  title = 'Angular'; 
+  h1Color = 'purple';
+
+  clickIt()  {
+    this.h1Color = this.h1Color === "purple" ? "blue" : "purple";
+  }
+}
